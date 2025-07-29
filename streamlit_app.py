858ca8fb23ec77c6ff3ce5b7601fff4985678848
LokeshDevalla <devallalokesh23@gmail.com>
2025-07-29 16:18:35 +0200

    Update streamlit_app.py
    added new api

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.text)
+smoothiefroot_response = requests.get(" https://fruityvice.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json() , use_container_widht=True)
